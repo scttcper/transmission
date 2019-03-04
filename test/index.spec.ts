1b5e3b8e07e4dfea8fa785df82b53015ab10300e
Scott Cooper <scttcper@gmail.com>
2019-03-03 20:03:32 -0800

    chore: split host into host and port in tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,7 +4,8 @@ import path from 'path';
 
 import { Transmission } from '../src/index';
 
-const host = 'http://localhost:9091/';
+const host = 'localhost';
+const port = 9091;
 const torrentFile = path.join(__dirname, '/ubuntu-18.04.1-desktop-amd64.iso.torrent');
 
 async function setupTorrent(transmission: Transmission) {
@@ -21,7 +22,7 @@ async function setupTorrent(transmission: Transmission) {
 
 describe('Transmission', () => {
   afterEach(async () => {
-    const transmission = new Transmission({ host });
+    const transmission = new Transmission({ host, port });
     const res = await transmission.listTorrents();
     // clean up all torrents
     for (const torrent of res.arguments.torrents) {
@@ -29,43 +30,43 @@ describe('Transmission', () => {
     }
   });
   it('should be instantiable', async () => {
-    const transmission = new Transmission({ host });
+    const transmission = new Transmission({ host, port });
     expect(transmission).toBeTruthy();
   });
   it('should add torrent from file path string', async () => {
-    const transmission = new Transmission({ host });
+    const transmission = new Transmission({ host, port });
     const res = await transmission.addTorrent(torrentFile);
     expect(res.result).toBe('success');
   });
   it('should add torrent from file buffer', async () => {
-    const transmission = new Transmission({ host });
+    const transmission = new Transmission({ host, port });
     const res = await transmission.addTorrent(fs.readFileSync(torrentFile));
     expect(res.result).toBe('success');
   });
   it('should add torrent from file contents base64', async () => {
-    const transmission = new Transmission({ host });
+    const transmission = new Transmission({ host, port });
     const contents = Buffer.from(fs.readFileSync(torrentFile)).toString('base64');
     const res = await transmission.addTorrent(contents);
     expect(res.result).toBe('success');
   });
   it('should get torrents', async () => {
-    const transmission = new Transmission({ host });
+    const transmission = new Transmission({ host, port });
     await setupTorrent(transmission);
     const res = await transmission.listTorrents(undefined, ['id']);
     expect(res.arguments.torrents).toHaveLength(1);
   });
   it('should remove torrent', async () => {
-    const transmission = new Transmission({ host });
+    const transmission = new Transmission({ host, port });
     const key = await setupTorrent(transmission);
     await transmission.removeTorrent(key, false);
   });
   it('should verify torrent', async () => {
-    const transmission = new Transmission({ host });
+    const transmission = new Transmission({ host, port });
     const key = await setupTorrent(transmission);
     await transmission.verifyTorrent(key);
   });
   it('should move in queue', async () => {
-    const transmission = new Transmission({ host });
+    const transmission = new Transmission({ host, port });
     const key = await setupTorrent(transmission);
     await transmission.queueUp(key);
     await transmission.queueDown(key);
@@ -73,7 +74,7 @@ describe('Transmission', () => {
     await transmission.queueBottom(key);
   });
   it('should report free space', async () => {
-    const transmission = new Transmission({ host });
+    const transmission = new Transmission({ host, port });
     const p = '/downloads';
     const res = await transmission.freeSpace(p);
     expect(res.result).toBe('success');
